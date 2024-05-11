fc1088bf696c354677dca1e4445639cc006ded33
Shahrom <aminzodashahrom8@gmail.com>
2024-05-11 12:45:39 +0500

    Added index.htmlvfaq-accordionjs file

---
+++ faq-accordionjs/main.js
@@ -0,0 +1,21 @@
+const accordingBtns = document.querySelectorAll(".according-btn");
+
+accordingBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+        this.classList.toggle("active");
+        const accordingDescription = this.nextElementSibling;
+        const plusIcon = this.querySelector(".plus-icon");
+        const minusIcon = this.querySelector(".minus-icon");
+
+        if (accordingDescription.style.maxHeight) {
+            accordingDescription.style.maxHeight = null;
+            plusIcon.style.display = "block";
+            minusIcon.style.display = "none";
+        } else {
+            accordingDescription.style.maxHeight =
+            accordingDescription.scrollHeight + "px";
+            plusIcon.style.display = "none";
+            minusIcon.style.display = "block";
+        }
+    })
+})
