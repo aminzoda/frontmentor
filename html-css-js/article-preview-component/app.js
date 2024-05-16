a53a5eb8722f4903b73943aaeaf27cbe17df9384
Shahrom <aminzodashahrom8@gmail.com>
2024-05-16 22:51:53 +0500

    commit

--- html-css-js/article-preview-component/app.js
+++ html-css-js/article-preview-component/app.js
@@ -10,44 +10,4 @@ removeShareBtn.addEventListener("click", () => {
   socials.classList.remove("clicked");
 });
 
-function shareFacebook() {
 
-  var url = "https://example.com";
-  
-  window.open(
-    "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url),
-    "_blank"
-  );
-}
-
-function shareTwitter() {
-  var url = "https://example.com";
-
-  var text = "Check out this cool website!";
-
-  window.open(
-    "https://twitter.com/intent/tweet?url=" +
-      encodeURIComponent(url) +
-      "&text=" +
-      encodeURIComponent(text),
-    "_blank"
-  );
-}
-
-function sharePinterest() {
-  var url = "https://example.com";
-
-  var imageUrl = "https://example.com/image.jpg";
-
-  var description = "Check out this cool website!";
-
-  window.open(
-    "https://www.pinterest.com/pin/create/button/?url=" +
-      encodeURIComponent(url) +
-      "&media=" +
-      encodeURIComponent(imageUrl) +
-      "&description=" +
-      encodeURIComponent(description),
-    "_blank"
-  );
-}
