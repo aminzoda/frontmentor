cf6658a34b4b847d5b5964546cda3f61f2c0aabc
Shahrom <aminzodashahrom8@gmail.com>
2024-05-19 00:30:14 +0500

    commit

---
+++ html-css-js/base-apparel-coming-soon/main.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".container form");
+const email = document.querySelector(".container form input[type=email]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let emailValue = email.value;
+  if (validateEmail(emailValue)) {
+    form.classList.remove("error");
+  } else {
+    form.classList.add("error");
+  }
+});
+
+function validateEmail(email) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(String(email).toLowerCase());
+}
