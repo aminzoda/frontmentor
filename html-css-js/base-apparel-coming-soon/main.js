b07ddcc18ff95f7494b6ace653f77dfbd518d0d7
Shahrom <aminzodashahrom8@gmail.com>
2024-05-20 18:37:09 +0500

    addition

--- html-css-js/base-apparel-coming-soon/main.js
+++ html-css-js/base-apparel-coming-soon/main.js
@@ -1,17 +1,19 @@
-const form = document.querySelector(".container form");
-const email = document.querySelector(".container form input[type=email]");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("emailForm");
+  const email = document.getElementById("email");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let emailValue = email.value;
-  if (validateEmail(emailValue)) {
-    form.classList.remove("error");
-  } else {
-    form.classList.add("error");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let emailValue = email.value;
+    if (validateEmail(emailValue)) {
+      form.classList.remove("error");
+    } else {
+      form.classList.add("error");
+    }
+  });
+
+  function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(String(email).toLowerCase());
   }
 });
-
-function validateEmail(email) {
-  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return regex.test(String(email).toLowerCase());
-}
