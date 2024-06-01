ebb00a1bbccec6e5dc93f2474ccd6655dac9d3ab
Shahrom <aminzodashahrom8@gmail.com>
2024-06-01 15:55:48 +0500

    commit

--- html-css-js/news-homepage/main.js
+++ html-css-js/news-homepage/main.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const navbar = document.querySelector("nav");
-  const openMenu = document.getElementById("menu-button");
-  const closeMenu = document.getElementById("menu-close");
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
 
-  openMenu.addEventListener("click", () => {
-    navbar.classList.add("open");
-  });
-
-  closeMenu.addEventListener("click", () => {
-    navbar.classList.remove("open");
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
   });
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const navbar = document.querySelector("nav");
+//   const openMenu = document.getElementById("menu-button");
+//   const closeMenu = document.getElementById("menu-close");
+
+//   openMenu.addEventListener("click", () => {
+//     navbar.classList.add("open");
+//   });
+
+//   closeMenu.addEventListener("click", () => {
+//     navbar.classList.remove("open");
+//   });
+// });
